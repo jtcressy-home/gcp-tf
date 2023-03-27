842c0bcf55ff40d575fbab4302b6e234248c4e97
Joel Cressy <joel.cressy@dottid.com>
2023-03-27 13:49:18 -0500

    artifact registry iam public

--- ar_debian_unifi-os.tf
+++ ar_debian_unifi-os.tf
@@ -6,3 +6,12 @@ resource "google_artifact_registry_repository" "unifi-os" {
   description   = "Custom Packages for Unifi OS >2.x"
   format        = "apt"
 }
+
+resource "google_artifact_registry_repository_iam_member" "unifi-os_public" {
+  provider   = google-beta
+  project    = data.google_project.jtcressy-net.project_id
+  location   = google_artifact_registry_repository.unifi-os.location
+  repository = google_artifact_registry_repository.unifi-os.name
+  role       = "roles/artifactregistry.reader"
+  member     = "allUsers"
+}
