657c3a78853c405f4d4f4cb81880cf0941b118f9
Joel Cressy <joel@jtcressy.net>
2023-04-03 16:16:51 -0500

    fix: grant gha-packer-builder access to compute default gsa

--- data.tf
+++ data.tf
@@ -2,6 +2,10 @@ data "google_project" "jtcressy-net" {
   project_id = "jtcressy-net-235001"
 }
 
+data "google_compute_default_service_account" "jtcressy-net" {
+  project = data.google_project.jtcressy-net.project_id
+}
+
 data "google_organization" "jtcressy-net" {
   domain = "jtcressy.net"
 }
--- gsa_gha-packer-builder.tf
+++ gsa_gha-packer-builder.tf
@@ -7,10 +7,16 @@ resource "google_service_account" "gha-packer-builder" {
 
 ## Service Account's Permissions
 
-resource "google_project_iam_member" "gha-packer-builder_artifact-writer" {
+resource "google_project_iam_member" "gha-packer-builder_computeInstanceAdmin" {
   project = data.google_project.jtcressy-net.project_id
   role    = "roles/compute.instanceAdmin"
   member  = "serviceAccount:${google_service_account.gha-packer-builder.email}"
+}
+
+resource "google_service_account_iam_member" "gha-packer-builder_computeDefault" {
+  service_account_id = data.google_compute_default_service_account.jtcressy-net.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.gha-packer-builder.email}"
 }
 
 ## Service Account's Workload Identity Mapping
